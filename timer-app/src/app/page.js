f43c8b8b7f078df5d41e4f85555b10a656b88eac
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2023-12-18 00:11:44 +1100

    setted up the semantic.css

--- timer-app/src/app/page.js
+++ timer-app/src/app/page.js
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import { v4 as uuidv4 } from 'uuid';
 import { newTimer, findById, renderElapsedString } from './helpers.js';
+import 'semantic-ui-css/semantic.min.css';
 
 
 const timers=[
@@ -92,9 +93,15 @@ const Timer=({title,project,id,elapsed,runningSince})=>{
         </div>
         <div className='extracontent'>
           <span className='right floated edit icon'>
+            <i className='edit icon'/>
+          </span>
+          <span className='right floated trash icon'>
             <i className='trash icon'/>
           </span>
         </div>
+      </div>
+      <div className = 'ui bottom attached blue basic button'>
+        Start
       </div>
     </div>
   )
